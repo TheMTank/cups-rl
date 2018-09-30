0c84638748e4bc8db121c7954caab9723a07207b
beduffy <benfduffy@gmail.com>
2018-09-30 21:24:56 +0100

    Fixed bounding boxes to use instance_detections2D attribute instead of colorBounds
    This avoided "the colored mug changing color as you place it and it not being in color_to_object_id matrix" bug. Put everything into main

--- bounding_boxes_and_test_reward.py
+++ bounding_boxes_and_test_reward.py
@@ -19,7 +19,7 @@ import ai2thor.controller
 def rgb2gray(rgb):
     return np.dot(rgb[..., :3], [0.299, 0.587, 0.114])
 
-def show_bounding_boxes(key_terms=None):
+def show_bounding_boxes_old(key_terms=None):
     fig2 = plt.figure()
     ax2 = fig2.add_subplot(111)
 
@@ -48,7 +48,29 @@ def show_bounding_boxes(key_terms=None):
             ax2.add_patch(rect)
     plt.show()
 
-def check_if_focus_and_close_enough_to_object_type(object_type='Mug'):
+def show_bounding_boxes(key_terms=None):
+    fig2 = plt.figure()
+    ax2 = fig2.add_subplot(111)
+
+    show_all = True if not key_terms else False
+
+    check_if = lambda x, name: True if x.lower() in name.lower() else False
+
+    plt.imshow(event.frame, interpolation='nearest')
+    for key, arr in event.instance_detections2D.items():
+        if show_all or any([check_if(term, key) for term in key_terms]):
+            x1, y1, x2, y2 = list(arr) #c_b_n['bounds']
+            # todo convert rgb color to hex and put below
+            print(key)
+            print(x1, y1, x2, y2)
+
+            rect = patches.Rectangle((x1, y1), abs(x2 - x1), abs(y2 - y1), linewidth=1,
+                                     edgecolor=matplotlib_colors[random.randint(0, len(matplotlib_colors) - 1)],
+                                     facecolor='none')
+            ax2.add_patch(rect)
+    plt.show()
+
+def check_if_focus_and_close_enough_to_object_type_old(object_type='Mug'):
     all_objects_for_object_type = [obj for obj in event.metadata['objects'] if obj['objectType'] == object_type]
     # distances_to_obj_type = [x['distance'] for x in all_objects_for_object_type]
 
@@ -69,6 +91,20 @@ def check_if_focus_and_close_enough_to_object_type(object_type='Mug'):
         x1, y1, x2, y2 = c_b_n_with_same_name_as_obj_id[0]['bounds']
 
         # check_if_focus_and_close_enough(x1, y1, x2, y2, distances_to_obj_type[idx])
+        bool_list.append(check_if_focus_and_close_enough(x1, y1, x2, y2, obj['distance']))
+
+    return sum(bool_list)
+
+def check_if_focus_and_close_enough_to_object_type(object_type='Mug'):
+    all_objects_for_object_type = [obj for obj in event.metadata['objects'] if obj['objectType'] == object_type]
+
+    bool_list = []
+    for idx, obj in enumerate(all_objects_for_object_type):
+        bounds = event.instance_detections2D.get(obj['objectId'])
+        if bounds is None:
+            continue
+
+        x1, y1, x2, y2 = bounds
         bool_list.append(check_if_focus_and_close_enough(x1, y1, x2, y2, obj['distance']))
 
     return sum(bool_list)
@@ -101,57 +137,58 @@ def close_enough(distance):
         return True
     return False
 
-# Kitchens: FloorPlan1 - FloorPlan30
-# Living rooms: FloorPlan201 - FloorPlan230
-# Bedrooms: FloorPlan301 - FloorPlan330
-# Bathrooms: FloorPLan401 - FloorPlan430
+if __name__ == '__main__':
+    # Kitchens: FloorPlan1 - FloorPlan30
+    # Living rooms: FloorPlan201 - FloorPlan230
+    # Bedrooms: FloorPlan301 - FloorPlan330
+    # Bathrooms: FloorPLan401 - FloorPlan430
 
-controller = ai2thor.controller.Controller()
-controller.start()
-controller.reset('FloorPlan28')
-event = controller.step(dict(action='Initialize', gridSize=0.25,
-                                 renderDepthImage=True,
-                                 renderClassImage=True,
-                                 renderObjectImage=True))
+    controller = ai2thor.controller.Controller()
+    controller.start()
+    controller.reset('FloorPlan28')
+    event = controller.step(dict(action='Initialize', gridSize=0.25,
+                                     renderDepthImage=True,
+                                     renderClassImage=True,
+                                     renderObjectImage=True))
 
-# Numpy Array - shape (width, height, channels), channels are in RGB order
-print(event.frame)
-print(event.frame.shape)
+    # Numpy Array - shape (width, height, channels), channels are in RGB order
+    print(event.frame)
+    print(event.frame.shape)
 
-event = controller.step(dict(action='MoveAhead'))
-event = controller.step(dict(action='RotateRight'))
-event = controller.step(dict(action='RotateRight'))
-event = controller.step(dict(action='MoveAhead'))
-event = controller.step(dict(action='MoveAhead'))
-event = controller.step(dict(action='RotateRight'))
-event = controller.step(dict(action='MoveAhead'))
-event = controller.step(dict(action='RotateLeft'))
-event = controller.step(dict(action='MoveAhead'))
-event = controller.step(dict(action='MoveAhead'))
-event = controller.step(dict(action='MoveAhead'))
-event = controller.step(dict(action='MoveAhead'))
-all_object_names = [obj['name'] for obj in event.metadata['objects']]
-visible_objects = [obj for obj in event.metadata['objects'] if obj['visible']]
+    # event = controller.step(dict(action='MoveAhead'))
+    event = controller.step(dict(action='RotateRight'))
+    event = controller.step(dict(action='RotateRight'))
+    event = controller.step(dict(action='MoveAhead'))
+    event = controller.step(dict(action='MoveAhead'))
+    event = controller.step(dict(action='RotateRight'))
+    event = controller.step(dict(action='MoveAhead'))
+    event = controller.step(dict(action='RotateLeft'))
+    event = controller.step(dict(action='MoveAhead'))
+    event = controller.step(dict(action='MoveAhead'))
+    event = controller.step(dict(action='MoveAhead'))
+    event = controller.step(dict(action='MoveAhead'))
+    all_object_names = [obj['name'] for obj in event.metadata['objects']]
+    visible_objects = [obj for obj in event.metadata['objects'] if obj['visible']]
 
-matplotlib_colors = ['b', 'g', 'r', 'c', 'm', 'y', 'k', 'w']
+    matplotlib_colors = ['b', 'g', 'r', 'c', 'm', 'y', 'k', 'w']
 
 
-# 98 75 126 109 - mug a bit to left middle centre
-# 165 75 194 109 - mug a bit to middle right bottom
-show_bounding_boxes()
-show_bounding_boxes(['mug', 'cup'])
-reward = check_if_focus_and_close_enough_to_object_type()
+    # 98 75 126 109 - mug a bit to left middle centre
+    # 165 75 194 109 - mug a bit to middle right bottom
+    show_bounding_boxes()
+    show_bounding_boxes(['mug', 'cup'])
+    reward = check_if_focus_and_close_enough_to_object_type()
+    #
+    # # Show preprocessed image
+    resolution = (64, 64)
+    img = skimage.transform.resize(event.frame, resolution)
+    img = img.astype(np.float32)
+    gray = rgb2gray(img)
+    gray_unsqueezed = np.expand_dims(gray, 0)
+    gray_3_channel = np.concatenate([gray_unsqueezed, gray_unsqueezed, gray_unsqueezed])
+    gray_3_channel = np.moveaxis(gray_3_channel, 0, 2)
+    plt.imshow(gray_3_channel)
+    plt.show()
 
-# Show preprocessed image
-resolution = (64, 64)
-img = skimage.transform.resize(event.frame, resolution)
-img = img.astype(np.float32)
-gray = rgb2gray(img)
-gray_unsqueezed = np.expand_dims(gray, 0)
-gray_3_channel = np.concatenate([gray_unsqueezed, gray_unsqueezed, gray_unsqueezed])
-gray_3_channel = np.moveaxis(gray_3_channel, 0, 2)
-plt.imshow(gray_3_channel)
-plt.show()
-
-# Can walk and step through environment interactively by copying commands and deciding when to show bounding boxes
-# import pdb;pdb.set_trace()
+    # Can walk and step through environment interactively by copying commands and deciding when to show bounding boxes
+    import pdb;pdb.set_trace()
