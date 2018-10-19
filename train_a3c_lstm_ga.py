Fixed massive bug of not loading new sentence and much more p_losses and v_losses print of average of these. Two new charts made showing these print last reward on done interaction=False MOVEMENT_REWARD_DISCOUNT up one order.

train_a3c_lstm_ga.py
@@ -22,7 +22,7 @@ from model import ActorCritic
 from a3c_lstm_ga_model import A3C_LSTM_GA
 import utils
 
-MOVEMENT_REWARD_DISCOUNT = -0.001
+MOVEMENT_REWARD_DISCOUNT = -0.01
 
 def ensure_shared_grads(model, shared_model):
     for param, shared_param in zip(model.parameters(),
@@ -39,7 +39,7 @@ def train_a3c_lstm_ga(rank, args, shared_model, counter, lock, optimizer=None):
     # env = envs.ThorWrapperEnv(current_object_type='Microwave', natural_language_instruction=True)
     # env = envs.ThorWrapperEnv(current_object_type='Microwave', dense_reward=True, natural_language_instruction=True)
     env = envs.ThorWrapperEnv(current_object_type='Microwave', natural_language_instruction=True, grayscale=False,
-                              max_episode_length=args.max_episode_length)
+                              max_episode_length=args.max_episode_length, interaction=False)
 
     env.seed(args.seed + rank)
 
@@ -76,6 +76,8 @@ def train_a3c_lstm_ga(rank, args, shared_model, counter, lock, optimizer=None):
     episode_total_rewards_list = []
     all_rewards_in_episode = []
     episode_lengths = []
+    p_losses = []
+    v_losses = []
     number_of_episodes = 0
     start = time.time()
     # plt.ion()
@@ -83,6 +85,7 @@ def train_a3c_lstm_ga(rank, args, shared_model, counter, lock, optimizer=None):
 
     total_length = args.total_length if args.total_length else 0
     episode_length = 0
+    num_backprops = 0
     while True:
         # Sync with the shared model
         model.load_state_dict(shared_model.state_dict())
@@ -137,18 +140,19 @@ def train_a3c_lstm_ga(rank, args, shared_model, counter, lock, optimizer=None):
                 counter.value += 1
 
             if done:
+                print('Episode over. Last reward: {}'.format(reward))
                 number_of_episodes += 1
                 episode_lengths.append(episode_length)
                 episode_length = 0
                 total_length -= 1
                 (image, instruction) = env.reset()
-                # todo is below needed for more sentences?
-                # instruction_idx = []
-                # for word in instruction.split(" "):
-                #     instruction_idx.append(env.word_to_idx[word])
-                # instruction_idx = np.array(instruction_idx)
-                # instruction_idx = torch.from_numpy(
-                #     instruction_idx).view(1, -1)
+                instruction_idx = []
+                for word in instruction.split(" "):
+                    instruction_idx.append(env.word_to_idx[word])
+                instruction_idx = np.array(instruction_idx)
+                instruction_idx = torch.from_numpy(
+                    instruction_idx).view(1, -1)
+                print('Instruction turned to tensors: ', instruction_idx)
 
                 # todo massive bug here with sums seeming wrong
                 total_reward_for_episode = sum(all_rewards_in_episode)
@@ -157,7 +161,7 @@ def train_a3c_lstm_ga(rank, args, shared_model, counter, lock, optimizer=None):
 
                 utils.create_plots(args.experiment_id, avg_reward_for_num_steps_list, total_reward_for_num_steps_list,
                                    number_of_episodes,
-                                   episode_total_rewards_list, episode_lengths, env, prob)
+                                   episode_total_rewards_list, episode_lengths, env, prob, p_losses, v_losses)
 
                 print('Episode number: {}. Total minutes elapsed: {:.3f}'.format(number_of_episodes,
                                                                                  (time.time() - start) / 60.0))
@@ -212,6 +216,25 @@ def train_a3c_lstm_ga(rank, args, shared_model, counter, lock, optimizer=None):
                 log_probs[i] * Variable(gae) - args.entropy_coef * entropies[i]
 
         optimizer.zero_grad()
+
+        p_losses.append(policy_loss.data[0, 0])
+        v_losses.append(value_loss.data[0, 0])
+
+        if len(p_losses) > 1000: # 1000 * 20 (args.num_steps) = 20000
+            # import pdb;pdb.set_trace()
+            num_backprops += 1
+            print(" ".join([
+                "Training thread: {}".format(rank),
+                "Num backprops: {}K".format(num_backprops),
+                "Avg policy loss: {}".format(np.mean(p_losses)),
+                "Avg value loss: {}".format(np.mean(v_losses))]))
+            # logging.info(" ".join([
+            #     "Training thread: {}".format(rank),
+            #     "Num iters: {}K".format(num_iters),
+            #     "Avg policy loss: {}".format(np.mean(p_losses)),
+            #     "Avg value loss: {}".format(np.mean(v_losses))]))
+            p_losses = []
+            v_losses = []
 
         (policy_loss + args.value_loss_coef * value_loss).backward()
         torch.nn.utils.clip_grad_norm(model.parameters(), args.max_grad_norm)

utils.py
@@ -9,7 +9,9 @@ mpl.use('Agg')  # or whatever other backend that you want
 import matplotlib.pyplot as plt
 
 def create_plots(experiment_id, avg_reward_for_num_steps_list, total_reward_for_num_steps_list, number_of_episodes,
-                 episode_total_rewards_list, episode_lengths, env, prob):
+                 episode_total_rewards_list, episode_lengths, env, prob, p_losses, v_losses):
+
+    # Plotting of ery bad chart of average of rewards
     num_elements_avg = 5
     mean = lambda x: sum(x) / len(x)
     avg_avg_rewards = [mean(avg_reward_for_num_steps_list[i:i + num_elements_avg]) for i in
@@ -20,7 +22,7 @@ def create_plots(experiment_id, avg_reward_for_num_steps_list, total_reward_for_
     x = range(len(avg_avg_rewards))
 
     assert len(total_reward_averages) == len(avg_avg_rewards)
-    fig = plt.figure(2)
+    fig = plt.figure(1)
     plt.clf()
     # fig1 = plt.gcf()
     plt.plot(x, avg_avg_rewards, label='label avg of avg rewards')
@@ -36,25 +38,26 @@ def create_plots(experiment_id, avg_reward_for_num_steps_list, total_reward_for_
     print('saved avg acc to: {}'.format(fp))
     plt.close(fig)
 
-    # next figure
-    fig = plt.figure(1)
+    # Plot otal Reward per episode chart
+    fig = plt.figure(2)
     plt.clf()
     x = range(len(episode_total_rewards_list))
     y = episode_total_rewards_list
     plt.plot(x, y)
-    plt.title('Total Reward per episode')
+    plt.title('Total Reward per episode Episode num: {}'.format(number_of_episodes))
     fp = '/home/beduffy/all_projects/ai2thor-testing/experiments/{}/a3c-total-reward-per-episode-{}.png'.format(
          experiment_id, number_of_episodes)
     plt.savefig(fp)
     print('saved total reward per episode to: {}'.format(fp))
     plt.close(fig)
 
+    # Plot episode lengths chart
     fig = plt.figure(3)
     plt.clf()
     x = range(len(episode_lengths))
     y = episode_lengths
     plt.plot(x, y)
-    plt.title('Episode lengths per episode')
+    plt.title('Episode lengths per episode Episode num: {}'.format(number_of_episodes))
     fp = '/home/beduffy/all_projects/ai2thor-testing/experiments/{}/episode-lengths-{}.png'.format(experiment_id,
          number_of_episodes)
     plt.savefig(fp)
@@ -62,6 +65,7 @@ def create_plots(experiment_id, avg_reward_for_num_steps_list, total_reward_for_
     plt.close(fig)
     # todo fill bottom underneath line so less coloured in is better
 
+    # Plot action probabilities chart
     x_tick_labels = [env.ACTION_SPACE[i]['action'] for i in range(len(env.ACTION_SPACE))]
     x_pos = np.arange(len(x_tick_labels))
     probabilities = prob.data.numpy()[0]
@@ -71,18 +75,47 @@ def create_plots(experiment_id, avg_reward_for_num_steps_list, total_reward_for_
     plt.bar(x_pos, probabilities, align='center', alpha=0.5)
     plt.xticks(x_pos, x_tick_labels, rotation='vertical')
     plt.ylabel('Probability')
-    plt.title('Action probabilities')
-    # plt.show() # for live mode but doesn't work
-    # plt.draw()
+    plt.title('Action probabilities Episode num: {}'.format(number_of_episodes))
     fp = '/home/beduffy/all_projects/ai2thor-testing/experiments/{}/action-probabilities-{}.png'.format(experiment_id,
                                                                                                    number_of_episodes)
     plt.savefig(fp)
     print('Saved episode lengths to: {}'.format(fp))
     plt.close(fig)
 
-    print(x_pos)
+    # print(x_pos)
     print(x_tick_labels)
     print(probabilities)
+
+    # plot policy loss charts
+    x1 = range(len(p_losses))
+    y1 = p_losses
+
+    fig = plt.figure(5)
+    plt.clf()
+    plt.plot(x1, y1)
+    plt.ylabel('Policy loss')
+    plt.title('Policy loss. Episode num: {}'.format(number_of_episodes))
+    fp = '/home/beduffy/all_projects/ai2thor-testing/experiments/{}/policy-loss-{}.png'.format(experiment_id,
+                                                                                                        number_of_episodes)
+    plt.savefig(fp)
+    print('Saved Policy loss to: {}'.format(fp))
+    plt.close(fig)
+
+    # plot value loss chart
+    x2 = range(len(v_losses))
+    y2 = v_losses
+
+    fig = plt.figure(6)
+    plt.clf()
+    plt.plot(x2, y2)
+    plt.ylabel('Value loss')
+    plt.title('Value loss. Episode num: {}'.format(number_of_episodes))
+    fp = '/home/beduffy/all_projects/ai2thor-testing/experiments/{}/value-loss-{}.png'.format(experiment_id,
+                                                                                                        number_of_episodes)
+    plt.savefig(fp)
+    print('Saved Value loss to: {}'.format(fp))
+    plt.close(fig)
+
 
 def save_checkpoint(state, experiment_id, filename, is_best=False):
     fp = '/home/beduffy/all_projects/ai2thor-testing/experiments/{}/{}'.format(experiment_id, filename)