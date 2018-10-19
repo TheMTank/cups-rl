cf1dc989f76dc2506aa419e73b1f5eba6729a32b
beduffy <benfduffy@gmail.com>
2018-10-19 23:56:28 +0100

    Added most of Fernando's changes
    typos, better var names (e.g. current task object, obj, episode_finished)
    Put random_walk back into examples but sys.path.append
    Updated README.md with requirements and added 2 __init__.pys
    dont print interaction objects if false
    Added openable objects

--- README.md
+++ README.md
@@ -1,12 +1,24 @@
 # ai2thor-experiments
 
 We created an OpenAI gym style wrapper for [ai2thor](http://ai2thor.allenai.org/) and show a random walk example below.
-We intend to experiment with different model-free and model-based algorithms within this repo with natural language 
-interfaces (e.g. "Pick up the cup", "Open the microwave") and work on representations which can transfer between tasks and environments.
+We intend to experiment with different model-free and model-based algorithms within this repo using a natural language
+ interface i.e. the environment returns sentences as part of its state e.g. "Pick up the cup", "Open the microwave". 
+ We also will work on representations which can transfer between tasks and environments.
 
+# Requirements
+
+* Python 3.6+
+* ai2thor (`pip install ai2thor`)
+* skimage
+
+To test to see if all functionality works run:
+
+`cd test`
+`python test_ai2thor_wrapper.py`
 
 # How to run
 
+`cd examples`
 `python random_walk.py`
 
 Simpler version below:
--- ai2thor_wrapper/envs.py
+++ ai2thor_wrapper/envs.py
@@ -24,7 +24,7 @@ POSSIBLE_ACTIONS = [
 ]
 
 class ThorWrapperEnv():
-    def __init__(self, scene_id='FloorPlan28', seed=None, task=0, max_episode_length=1000, current_object_type='Mug',
+    def __init__(self, scene_id='FloorPlan28', seed=None, task=0, max_episode_length=1000, current_task_object='Mug',
                  grayscale=True, interaction=True, config_path='config_example.ini', movement_reward=0):
         # Loads config file from path relative to ai2thor_wrapper python package folder
         config_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), config_path)
@@ -45,19 +45,20 @@ class ThorWrapperEnv():
                                                renderClassImage=True,
                                                renderObjectImage=True))
 
-        self.current_object_type = current_object_type
+        self.current_task_object = current_task_object
         self.max_episode_length = max_episode_length
         self.t = 0
         self.done = False
         self.task = task
         self.movement_reward = movement_reward
+        self.interaction = interaction
 
         # action dictionary from int to dict action to pass to event.controller.step()
         self.ACTION_SPACE = {}
         for idx, action_str in enumerate(POSSIBLE_ACTIONS):
             self.ACTION_SPACE[idx] = dict(action=action_str)
 
-        if not interaction:
+        if not self.interaction:
             interaction_actions = ['OpenObject', 'CloseObject', 'PickupObject', 'PutObject']
             for idx in range(len(POSSIBLE_ACTIONS)):
                 if self.ACTION_SPACE[idx]['action'] in interaction_actions:
@@ -69,8 +70,11 @@ class ThorWrapperEnv():
         # acceptable objects taken from config.ini file. Stripping to allow spaces
         self.pickup_object_types = [x.strip() for x in self.config['ENV_SPECIFIC']['PICKUP_OBJECTS'].split(',')]
         self.receptacle_object_types = [x.strip() for x in self.config['ENV_SPECIFIC']['ACCEPTABLE_RECEPTACLES'].split(',')]
-        print('Objects that can be picked up: \n{}'.format(self.pickup_object_types))
-        print('Objects that allow objects placed into/onto them (receptacles): \n{}'.format(self.receptacle_object_types))
+        self.openable_objects = [x.strip() for x in self.config['ENV_SPECIFIC']['OPENABLE_OBJECTS'].split(',')]
+        if self.interaction:
+            print('Objects that can be picked up: \n{}'.format(self.pickup_object_types))
+            print('Objects that allow objects placed into/onto them (receptacles): \n{}'.format(self.receptacle_object_types))
+            print('Objects that can be opened: \n{}'.format(self.openable_objects))
 
         self.grayscale = grayscale
         self.resolution = (128, 128)  # (64, 64)
@@ -85,55 +89,60 @@ class ThorWrapperEnv():
     def step(self, action_int):
         if self.ACTION_SPACE[action_int]['action'] == 'PickupObject':
             if len(self.event.metadata['inventoryObjects']) == 0:
-                for o in self.event.metadata['objects']:
+                for obj in self.event.metadata['objects']:
                     # loop through objects that are visible, pickupable and there is a bounding box visible
-                    if o['visible'] and o['pickupable'] and o['objectType'] in self.pickup_object_types \
-                            and o['objectId'] in self.event.instance_detections2D:
-                        object_id = o['objectId']
-                        object_type = o['objectType']
+                    if obj['visible'] and \
+                           obj['pickupable'] and \
+                           obj['objectType'] in self.pickup_object_types and \
+                           obj['objectId'] in self.event.instance_detections2D:
+                        object_id = obj['objectId']
+                        object_type = obj['objectType']
                         self.event = self.controller.step(
                             dict(action='PickupObject', objectId=object_id), raise_for_failure=True)
-                        if object_type == self.current_object_type:
+                        if object_type == self.current_task_object:
                             self.goal_objects_collected_and_placed.append(object_id)
                         print('Picked up', self.event.metadata['inventoryObjects'])
                         break
         elif self.ACTION_SPACE[action_int]['action'] == 'PutObject':
             if len(self.event.metadata['inventoryObjects']) > 0:
-                for o in self.event.metadata['objects']:
+                for obj in self.event.metadata['objects']:
                     # loop through receptacles
-                    if o['visible'] and o['receptacle'] and \
-                            o['objectType'] in self.receptacle_object_types and \
-                            len(o['receptacleObjectIds']) < o['receptacleCount']:
+                    if obj['visible'] and \
+                            obj['receptacle'] and \
+                            obj['objectType'] in self.receptacle_object_types and \
+                       len(obj['receptacleObjectIds']) < obj['receptacleCount']:
                         # todo might still crash.
                         inventory_object_id = self.event.metadata['inventoryObjects'][0]['objectId']
                         inventory_object_type = self.event.metadata['inventoryObjects'][0]['objectType']
 
                         self.event = self.controller.step(dict(action='PutObject', objectId=inventory_object_id,
-                                                               receptacleObjectId=o['objectId']))#,
+                                                               receptacleObjectId=obj['objectId']))#,
                                                                #raise_for_failure=True)
-                        if inventory_object_type == self.current_object_type:
+                        if inventory_object_type == self.current_task_object:
                             self.goal_objects_collected_and_placed.remove(inventory_object_id)
-                        print('Placed', inventory_object_id, ' onto', o['objectId'], ' Inventory: ',
+                        print('Placed', inventory_object_id, ' onto', obj['objectId'], ' Inventory: ',
                               self.event.metadata['inventoryObjects'])
                         break
         elif self.ACTION_SPACE[action_int]['action'] == 'OpenObject':
-            for o in self.event.metadata['objects']:
+            for obj in self.event.metadata['objects']:
                 # loop through objects that are visible, openable, closed
-                if o['visible'] and o['openable'] and not o['isopen'] and o['objectType'] == 'Microwave' \
-                        and o['objectId'] in self.event.instance_detections2D:
-                    print('Opened', o['objectId'])
+                if obj['visible'] and obj['openable'] and \
+                        not obj['isopen'] and obj['objectType'] in self.openable_objects and \
+                        obj['objectId'] in self.event.instance_detections2D:
+                    print('Opened', obj['objectId'])
 
                     self.event = self.controller.step(
-                        dict(action='OpenObject', objectId=o['objectId']), raise_for_failure=True)
+                        dict(action='OpenObject', objectId=obj['objectId']), raise_for_failure=True)
                     break
         elif self.ACTION_SPACE[action_int]['action'] == 'CloseObject':
-            for o in self.event.metadata['objects']:
+            for obj in self.event.metadata['objects']:
                 # loop through objects that are visible, openable, open
-                if o['visible'] and o['openable'] and o['isopen'] and o['objectType'] == 'Microwave' \
-                        and o['objectId'] in self.event.instance_detections2D:
-                    print('Closed', o['objectId'])
+                if obj['visible'] and obj['openable'] and obj['isopen'] and \
+                        obj['objectType'] in self.openable_objects and \
+                        obj['objectId'] in self.event.instance_detections2D:
+                    print('Closed', obj['objectId'])
                     self.event = self.controller.step(
-                        dict(action='CloseObject', objectId=o['objectId']), raise_for_failure=True)
+                        dict(action='CloseObject', objectId=obj['objectId']), raise_for_failure=True)
                     break
         else:
             action = self.ACTION_SPACE[action_int]
@@ -142,7 +151,7 @@ class ThorWrapperEnv():
         self.t += 1
         state = self.preprocess(self.event.frame)
         reward = self.calculate_reward(self.done)
-        self.done = self.is_episode_finished()
+        self.done = self.episode_finished()
         return state, reward, self.done
 
     def preprocess(self, img):
@@ -172,7 +181,7 @@ class ThorWrapperEnv():
 
         return reward
 
-    def is_episode_finished(self):
+    def episode_finished(self):
         if self.max_episode_length and self.t >= self.max_episode_length:
             print('Reached maximum episode length: t: {}'.format(self.t))
             return True
