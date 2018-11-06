29e0fd3b9eb28021770a2cd621f14060e3502ec3
beduffy <benfduffy@gmail.com>
2018-11-06 23:40:48 +0000

    Added many changes to train.py. For example. No plotting,
    cleaner episode finished log. Removed reward clamping. Monitoring with 4 lists
    Total length

--- algorithms/a3c/train.py
+++ algorithms/a3c/train.py
@@ -1,10 +1,11 @@
 """
-Adapted from: https://github.com/ikostrikov/pytorch-a3c/blob/master/train.py
+Adapted from https://github.com/ikostrikov/pytorch-a3c
 """
 
 import torch
 import torch.nn.functional as F
 import torch.optim as optim
+from torch.autograd import Variable
 
 # from envs import create_atari_env
 from gym_ai2thor.envs.ai2thor_env import AI2ThorEnv
@@ -23,10 +24,10 @@ def train(rank, args, shared_model, counter, lock, optimizer=None):
     torch.manual_seed(args.seed + rank)
 
     # env = create_atari_env(args.env_name)
-    env = AI2ThorEnv()
+    env = AI2ThorEnv(config_dict={'max_episode_length': args.max_episode_length})
     env.seed(args.seed + rank)
 
-    model = ActorCritic(env.observation_space.shape[0], env.action_space)
+    model = ActorCritic(env.observation_space.shape[0], env.action_space.n)
 
     if optimizer is None:
         optimizer = optim.Adam(shared_model.parameters(), lr=args.lr)
@@ -37,16 +38,23 @@ def train(rank, args, shared_model, counter, lock, optimizer=None):
     state = torch.from_numpy(state)
     done = True
 
+    # monitoring
+    total_reward_for_num_steps_list = []
+    episode_total_rewards_list = []
+    all_rewards_in_episode = []
+    avg_reward_for_num_steps_list = []
+
+    total_length = 0
     episode_length = 0
     while True:
         # Sync with the shared model
         model.load_state_dict(shared_model.state_dict())
         if done:
-            cx = torch.zeros(1, 256)
-            hx = torch.zeros(1, 256)
+            cx = Variable(torch.zeros(1, 256))
+            hx = Variable(torch.zeros(1, 256))
         else:
-            cx = cx.detach()
-            hx = hx.detach()
+            cx = Variable(cx.data)
+            hx = Variable(hx.data)
 
         values = []
         log_probs = []
@@ -55,61 +63,77 @@ def train(rank, args, shared_model, counter, lock, optimizer=None):
 
         for step in range(args.num_steps):
             episode_length += 1
-            value, logit, (hx, cx) = model((state.unsqueeze(0),
-                                            (hx, cx)))
-            prob = F.softmax(logit, dim=-1)
-            log_prob = F.log_softmax(logit, dim=-1)
+            total_length += 1
+            value, logit, (hx, cx) = model((Variable(state.unsqueeze(0).float()), (hx, cx)))
+            prob = F.softmax(logit)
+            log_prob = F.log_softmax(logit)
             entropy = -(log_prob * prob).sum(1, keepdim=True)
             entropies.append(entropy)
 
-            action = prob.multinomial(num_samples=1).detach()
-            log_prob = log_prob.gather(1, action)
+            action = prob.multinomial(num_samples=1).data
+            log_prob = log_prob.gather(1, Variable(action))
 
-            state, reward, done, _ = env.step(action.numpy())
+            action_int = action.numpy()[0][0].item()
+            state, reward, done, _ = env.step(action_int)
+
             done = done or episode_length >= args.max_episode_length
-            reward = max(min(reward, 1), -1)
 
             with lock:
                 counter.value += 1
 
             if done:
                 episode_length = 0
+                total_length -= 1
+                total_reward_for_episode = sum(all_rewards_in_episode)
+                episode_total_rewards_list.append(total_reward_for_episode)
                 state = env.reset()
+                print('Episode Over. Total Length: {}. Total reward for episode: {}'.format(
+                                            total_length,  total_reward_for_episode))
+                print('Step no: {}. total length: {}'.format(episode_length, total_length))
 
             state = torch.from_numpy(state)
             values.append(value)
             log_probs.append(log_prob)
             rewards.append(reward)
+            all_rewards_in_episode.append(reward)
 
             if done:
                 break
 
+        # Backprop and no interaction with environment below,
         R = torch.zeros(1, 1)
-        if not done:
-            value, _, _ = model((state.unsqueeze(0), (hx, cx)))
-            R = value.detach()
+        if not done:  # to change last reward to predicted value to ....
+            value, _, _ = model((Variable(state.unsqueeze(0).float()), (hx, cx)))
+            R = value.data
 
-        values.append(R)
+        total_reward_for_num_steps = sum(rewards)
+        total_reward_for_num_steps_list.append(total_reward_for_num_steps)
+        avg_reward_for_num_steps = total_reward_for_num_steps / len(rewards)
+        avg_reward_for_num_steps_list.append(avg_reward_for_num_steps)
+
+        values.append(Variable(R))
         policy_loss = 0
         value_loss = 0
+        # import pdb;pdb.set_trace() # good place to breakpoint to see training cycle
+        R = Variable(R)
         gae = torch.zeros(1, 1)
         for i in reversed(range(len(rewards))):
             R = args.gamma * R + rewards[i]
             advantage = R - values[i]
             value_loss = value_loss + 0.5 * advantage.pow(2)
 
-            # Generalized Advantage Estimataion
+            # Generalized Advantage Estimation
             delta_t = rewards[i] + args.gamma * \
-                values[i + 1] - values[i]
+                values[i + 1].data - values[i].data
             gae = gae * args.gamma * args.tau + delta_t
 
             policy_loss = policy_loss - \
-                log_probs[i] * gae.detach() - args.entropy_coef * entropies[i]
+                log_probs[i] * Variable(gae) - args.entropy_coef * entropies[i]
 
         optimizer.zero_grad()
 
         (policy_loss + args.value_loss_coef * value_loss).backward()
-        torch.nn.utils.clip_grad_norm_(model.parameters(), args.max_grad_norm)
+        torch.nn.utils.clip_grad_norm(model.parameters(), args.max_grad_norm)
 
         ensure_shared_grads(model, shared_model)
         optimizer.step()
