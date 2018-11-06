3f9bb83907e96874a810c22b245fc14032f8b3cf
beduffy <benfduffy@gmail.com>
2018-11-06 22:19:36 +0000

    Added atari envs (could have to option to try both in future) and test.py (didn't realise it slept)

---
+++ algorithms/a3c/test.py
@@ -0,0 +1,73 @@
+"""
+Adapted from: https://github.com/ikostrikov/pytorch-a3c/blob/master/test.py
+"""
+import time
+from collections import deque
+
+import torch
+import torch.nn.functional as F
+
+# from envs import create_atari_env
+from gym_ai2thor.envs.ai2thor_env import AI2ThorEnv
+from model import ActorCritic
+
+
+def test(rank, args, shared_model, counter):
+    torch.manual_seed(args.seed + rank)
+
+    # env = create_atari_env(args.env_name)
+    env = AI2ThorEnv()
+    env.seed(args.seed + rank)
+
+    model = ActorCritic(env.observation_space.shape[0], env.action_space)
+
+    model.eval()
+
+    state = env.reset()
+    state = torch.from_numpy(state)
+    reward_sum = 0
+    done = True
+
+    start_time = time.time()
+
+    # a quick hack to prevent the agent from stucking
+    actions = deque(maxlen=100)
+    episode_length = 0
+    while True:
+        episode_length += 1
+        # Sync with the shared model
+        if done:
+            model.load_state_dict(shared_model.state_dict())
+            cx = torch.zeros(1, 256)
+            hx = torch.zeros(1, 256)
+        else:
+            cx = cx.detach()
+            hx = hx.detach()
+
+        with torch.no_grad():
+            value, logit, (hx, cx) = model((state.unsqueeze(0), (hx, cx)))
+        prob = F.softmax(logit, dim=-1)
+        action = prob.max(1, keepdim=True)[1].numpy()
+
+        state, reward, done, _ = env.step(action[0, 0])
+        done = done or episode_length >= args.max_episode_length
+        reward_sum += reward
+
+        # a quick hack to prevent the agent from stucking
+        actions.append(action[0, 0])
+        if actions.count(actions[0]) == actions.maxlen:
+            done = True
+
+        if done:
+            print("Time {}, num steps {}, FPS {:.0f}, episode reward {}, episode length {}".format(
+                time.strftime("%Hh %Mm %Ss",
+                              time.gmtime(time.time() - start_time)),
+                counter.value, counter.value / (time.time() - start_time),
+                reward_sum, episode_length))
+            reward_sum = 0
+            episode_length = 0
+            actions.clear()
+            state = env.reset()
+            time.sleep(60)
+
+        state = torch.from_numpy(state)
