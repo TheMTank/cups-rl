3acecaf20a3618de5a11c33642c8f65e360f4bba
beduffy <benfduffy@gmail.com>
2018-11-17 16:02:17 +0000

    Added A3C GA model to model.py and removed 3 files I won't need (good-experiment-ids.txt, model.py, check_latest_plots)
    Moved sample event metadata to gym_ai2thor folder

--- algorithms/a3c/model.py
+++ algorithms/a3c/model.py
@@ -111,3 +111,92 @@ class ActorCritic(torch.nn.Module):
         x = hx
 
         return self.critic_linear(x), self.actor_linear(x), (hx, cx)
+
+class A3C_LSTM_GA(torch.nn.Module):
+
+    def __init__(self, num_input_channels, num_outputs, frame_dim):
+        super(A3C_LSTM_GA, self).__init__()
+
+        # Image Processing
+        self.conv1 = nn.Conv2d(num_input_channels, 128, kernel_size=8, stride=4)
+        self.conv2 = nn.Conv2d(128, 64, kernel_size=4, stride=2)
+        self.conv3 = nn.Conv2d(64, 64, kernel_size=4, stride=2)
+
+        # Instruction Processing
+        self.gru_hidden_size = 256
+        # self.input_size = args.input_size # todo
+        # self.input_size = 4 # todo change, currently "go to the microwave" 4 words
+        # self.input_size = 5 # #'Turn left 3 times', 'Turn right 3 times'
+        # self.input_size = 6  #['Go and look at microwave', 'Go and look at cup']
+        self.input_size = 2  # ['microwave', 'cup']
+        self.embedding = nn.Embedding(self.input_size, 32)
+        self.gru = nn.GRU(32, self.gru_hidden_size)
+
+        # Gated-Attention layers
+        self.attn_linear = nn.Linear(self.gru_hidden_size, 64)
+
+        # Time embedding layer, helps in stabilizing value prediction
+        self.time_emb_dim = 32
+        self.time_emb_layer = nn.Embedding(
+                # args.max_episode_length+1, # todo feed in episode length
+                1000 + 1,
+                self.time_emb_dim)
+
+        # A3C-LSTM layers
+        # self.linear = nn.Linear(64 * 8 * 17, 256)
+        self.linear = nn.Linear(64 * 6 * 6, 256) # todo maybe 150, 150 res
+        self.lstm = nn.LSTMCell(256, 256)
+        self.critic_linear = nn.Linear(256 + self.time_emb_dim, 1)
+        self.actor_linear = nn.Linear(256 + self.time_emb_dim, num_outputs)
+
+        # Initializing weights
+        self.apply(weights_init)
+        self.actor_linear.weight.data = normalized_columns_initializer(
+            self.actor_linear.weight.data, 0.01)
+        self.actor_linear.bias.data.fill_(0)
+        self.critic_linear.weight.data = normalized_columns_initializer(
+            self.critic_linear.weight.data, 1.0)
+        self.critic_linear.bias.data.fill_(0)
+
+        self.lstm.bias_ih.data.fill_(0)
+        self.lstm.bias_hh.data.fill_(0)
+        self.train()
+
+    def forward(self, inputs):
+        x, input_inst, (tx, hx, cx) = inputs
+
+        # Get the image representation
+        x = F.relu(self.conv1(x.double()))
+        x = F.relu(self.conv2(x))
+        x_image_rep = F.relu(self.conv3(x))
+
+        # Get the instruction representation
+        encoder_hidden = torch.zeros(1, 1, self.gru_hidden_size).double()
+        for i in range(input_inst.data.size(1)):
+
+            # todo first unsqueeze should be sequence length!!!
+            # but for now im gonna try just one word. todo check when i bring multi word sentences back
+            word_embedding = self.embedding(input_inst[0, i]).unsqueeze(0).unsqueeze(0)  # crazy todo
+            word_embedding = word_embedding.expand(input_inst.data.size(1), -1, -1) # only change seq len dimension
+            _, encoder_hidden = self.gru(word_embedding, encoder_hidden) # todo import pdb;pdb.set_trace(). see if this works nad two unsqueezes were wrong?
+        x_instr_rep = encoder_hidden.view(encoder_hidden.size(1), -1)
+        # todo try MLP above instead or just one layer
+
+        # Get the attention vector from the instruction representation
+        x_attention = F.sigmoid(self.attn_linear(x_instr_rep))
+
+        # Gated-Attention
+        x_attention = x_attention.unsqueeze(2).unsqueeze(3)
+        # x_attention = x_attention.expand(1, 64, 8, 17)
+        x_attention = x_attention.expand(1, 64, 6, 6)
+        assert x_image_rep.size() == x_attention.size()
+        x = x_image_rep*x_attention
+        x = x.view(x.size(0), -1)
+
+        # A3C-LSTM
+        x = F.relu(self.linear(x))
+        hx, cx = self.lstm(x, (hx.double(), cx.double()))
+        time_emb = self.time_emb_layer(tx)
+        x = torch.cat((hx, time_emb.view(-1, self.time_emb_dim)), 1)
+
+        return self.critic_linear(x), self.actor_linear(x), (hx, cx)
