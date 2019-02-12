Fixed off by one episode_length time embedding by incrementing after step, moved 3 ai2thor tests to ai2thor_examples.py, default_config PickUpTask and movement_reward in others,

algorithms/a3c/model.py
@@ -55,7 +55,6 @@ def normalized_columns_initializer(weights, std=1.0):
     out *= std / torch.sqrt(out.pow(2).sum(1, keepdim=True).expand_as(out)) # todo expand_as(out) double check
     return out
 
-
 def weights_init(m):
     classname = m.__class__.__name__
     if classname.find('Conv') != -1:
@@ -95,7 +94,7 @@ class ActorCritic(torch.nn.Module):
         # assumes square image
         self.lstm_cell_size = calculate_lstm_input_size_for_A3C(frame_dim)
 
-        self.lstm = nn.LSTMCell(self.lstm_cell_size, 256)  # for any square input
+        self.lstm = nn.LSTMCell(self.lstm_cell_size, 256)
 
         self.critic_linear = nn.Linear(256, 1)
         self.actor_linear = nn.Linear(256, num_outputs)
@@ -126,6 +125,7 @@ class ActorCritic(torch.nn.Module):
 
         return self.critic_linear(x), self.actor_linear(x), (hx, cx)
 
+
 class A3C_LSTM_GA(torch.nn.Module):
     """
     Very similar to the above ActorCritic but has Gated Attention (GA) and processes an instruction
@@ -133,6 +133,7 @@ class A3C_LSTM_GA(torch.nn.Module):
     of the input image given the instruction e.g. instruction "Go to the red cup" and a filter
     could learn and language ground itself in the meaning of "red" with a filter that learns this
     mapping. There is also a time embedding layer to help stabilize value prediction.
+    Only 3 conv layers compared to ActorCritic's 4 layers.
     """
     def __init__(self, num_input_channels, num_outputs, frame_dim, vocab_size, episode_length):
         super(A3C_LSTM_GA, self).__init__()
@@ -158,7 +159,7 @@ class A3C_LSTM_GA(torch.nn.Module):
         # Time embedding layer, helps in stabilizing value prediction
         self.time_emb_dim = 32
         self.time_emb_layer = nn.Embedding(
-                episode_length + 1, # todo check if it works and why +1? because we move 1 after steps. off by one,
+                episode_length,
                 self.time_emb_dim)
 
         # A3C-LSTM layers